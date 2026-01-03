9c9c2e6209a46d7f418648997aadadd2ae5f6871
gelni0221 <antipolonigel@gmail.com>
2026-01-03 21:14:49 +0800

    another tests lessons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import {Test,Test1, Test2, Test3, Test4} from './mini_projects/test1';
+import {Test,Test1, Test2, Test3, Test4, Test5,Test6,Test7} from './mini_projects/test1';
 const App = () =>{
-return <Test4 isTrue={true} />;
+return <Test7 />;
 };
 
 export default App;
--- frontend/src/mini_projects/test1.js
+++ frontend/src/mini_projects/test1.js
@@ -1,5 +1,5 @@
 import './test1.css';
-
+import { useState, useRef  } from 'react';
 
 // BASIC REACT COMPONENT WITH IF ELSE AND HTML
 // truth false values if isLoggedIn is true then welcome else not welcome 
@@ -83,6 +83,81 @@ export const Test4 = ({isTrue}) => {
 
   return(
   <>{isTrue ? <Test1/> : <Test2/>}  </>
+  // IF LEFT SIDE TRUE DO RIGHT SIDE WITH &&
+  // <>{isTrue && <Test1/>}  </>
+  );
+}
+
+export const Test5 = () => {
+// FAKE DATA - OFC USE SQL DATA INSTEAD IF THIS
+  const accounts = [
+    {id: 1, username: 'admin', password: 'admin'},
+    {id: 2, username: 'admin1', password: 'admin1'},
+    {id: 3, username: 'admin2', password: 'admin2'},
+  ];
+
+  return(
+    // YOU USE MAP TO ITERATE THE ACCOUNT DATA
+    <>
+    <h1>Accounts: </h1>
+    <or>
+    {accounts.map((account) => <li key={account.id}>
+      Username: {account.username} |
+      Password: {account.password}
+      </li>)}
+
+    </or>
+    </>
+  );
+}
+
+export const Test6 = () => {
+    const [name, setName] = useState("");
+
+  function handleChange(e) {
+    // e.target.value = the current content of the input field
+    setName(e.target.value);
+  }
+  return(
+    <form>
+      <label>Enter your name:
+        <input
+          type="text" 
+          value={name}
+          // onchange makes it dynamic
+          onChange={handleChange}
+        />
+      </label>
+      <p>Current value: {name}</p>
+    </form>
+  );
+}
+
+export const Test7 = () => {
+
+  const [name, setName] = useState("");
+  function handleClick(e) {
+    setName(inputRef.current.value);
+  }
+  const inputRef = useRef(null);
+// useRef is a React hook that gives you a persistent reference to something that doesn’t trigger a re-render when it changes.
+// Think of it like a box you can put a value in, and React will remember it between renders.
+  return(
+    <form>
+      <label>Change your Name <br />
+      <input 
+      type="text"     
+      ref= {inputRef}
+      ></input>
+      </label>
+      
+      <button
+      onClick={handleClick}
+      type="button"
+      ></button>
+      <p>{name}</p>
+    </form>
+
   );
 }
 
